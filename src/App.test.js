bc1707595accbef2a3a3ee6daa2725c2c38c5a6d
Nik 'Fire Eater' Krimm <nik@thoughful.ai>
2022-07-26 12:52:38 -0500

    slight improvements to the initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('Renders "Welcome React Chicago" as headline', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome React Chicago/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeStatement = screen.getByText(/Welcome React Chicago/i);
+  expect(welcomeStatement).toBeInTheDocument();
 });
