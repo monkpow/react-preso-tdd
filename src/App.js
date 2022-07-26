8bdb97a0f2039e3aba5fdb8f3f2ffaafd32205eb
Nik 'Fire Eater' Krimm <nik@thoughful.ai>
2022-07-26 13:54:42 -0500

    test for dark mode passing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
+import { useState } from 'react'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const updateDarkMode = (event) => {
+    setDarkMode(event.target.checked);
+  }
+
+  const applicationClasses = () => {
+    let classes = ["App"];
+    return (darkMode ? classes.concat('dark-mode') : classes).join(" ")
+  }
+
   return (
-    <div className="App">
+    <div className={applicationClasses()} data-testid="main-body-styles">
       <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +32,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <label>
+          <input type="checkbox" checked={darkMode} onChange={updateDarkMode} />
+          Dark Mode
+        </label>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
 test('Renders "Welcome React Chicago" as headline', () => {
   render(<App />);
   const welcomeStatement = screen.getByText(/Welcome React Chicago/i);
   expect(welcomeStatement).toBeInTheDocument();
+});
+
+test("Application renders in dark-mode by default", () => {
+  render(<App />);
+  const element = screen.getByTestId('main-body-styles');
+  expect(element).toHaveClass('dark-mode');
+});
+
+test("Application allows user to toggle dark-mode", () => {
+  render(<App />)
+  const element = screen.getByTestId('main-body-styles');
+  expect(element).toHaveClass('dark-mode');
+  fireEvent.click(screen.getByText('Dark Mode'))
+  expect(element).not.toHaveClass('dark-mode');
+  fireEvent.click(screen.getByText('Dark Mode'))
+  expect(element).toHaveClass('dark-mode');
 });
