a991e367fe479dc96bde19fb3198fbffad3eff5f
Nik 'Fire Eater' Krimm <nik@thoughful.ai>
2022-07-26 12:51:25 -0500

    first failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders "Welcome React Chicago" as headline', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome React Chicago/i);
   expect(linkElement).toBeInTheDocument();
 });
